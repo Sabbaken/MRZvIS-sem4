8b8e44b7b5531527234763c7e8e92a6850558c06
Sabbaken <kirillverenih@ya.ru>
2020-02-24 18:50:17 +0300

    Add summ test

--- laba_1/tests/test1.cpp
+++ laba_1/tests/test1.cpp
@@ -17,19 +17,23 @@ TEST(BinaryCalculatorTests, dec_number_to_bin_conversion_test) {
     EXPECT_EQ(_6_bin, calc._10_to_2_(6));
 }
 
-TEST(BinaryComputerTests, bin_to_additional_code_conversion_test) {
+TEST(BinaryCalculatorTests, summ_test) {
     BinaryCalculator calc = BinaryCalculator();
 
-    std::vector<int> _1_bin = {1, 0, 0, 0};;
-    std::vector<int> _4_bin = {0, 0, 1, 0};;
-    std::vector<int> _1_bin_additional_code = {1, 0, 0, 0};;
-    std::vector<int> _4_bin_additional_code = {0, 0, 1, 0};;
+    std::vector<int> _1_bin = {1, 0, 0, 0};
+    std::vector<int> _2_bin = {0, 1, 0, 0};
+    std::vector<int> _4_bin = {0, 0, 1, 0};
+    std::vector<int> _5_bin = {1, 0, 1, 0};
+    std::vector<int> _6_bin = {0, 1, 1, 0};
+    std::vector<int> _minus_1_bin = {1, 0, 0, 1};
+    std::vector<int> _minus_2_bin = {0, 1, 0, 1};
+    std::vector<int> _minus_3_bin = {1, 1, 0, 1};
 
-    EXPECT_EQ(_1_bin_additional_code, calc._2_to_additional_code(_1_bin));
-    EXPECT_EQ(_4_bin_additional_code, calc._2_to_additional_code(_4_bin));
+    EXPECT_EQ(_2_bin, calc.binaryAddition(_1_bin, _1_bin));
+    EXPECT_EQ(_minus_3_bin, calc.binaryAddition(_4_bin, _minus_1_bin));
 }
 
-TEST(BinaryComputerTests, bin_inversion_test) {
+TEST(BinaryCalculatorTests, bin_inversion_test) {
     BinaryCalculator calc = BinaryCalculator();
 
     std::vector<int> _1_bin = {1, 0, 0, 0};
@@ -45,15 +49,18 @@ TEST(BinaryComputerTests, bin_inversion_test) {
     EXPECT_EQ(_minus_3_bin_inverted, calc._2_to_inverted_code(_minus_3_bin));
 }
 
-TEST(BinaryComputerTests, bin_additional_test) {
+TEST(BinaryCalculatorTests, bin_to_additional_code_conversion_test) {
     BinaryCalculator calc = BinaryCalculator();
 
-    std::vector<int> _2_additional = {0, 1, 0, 0};
-    std::vector<int> _2_binary = {0, 1, 0, 0};
+    std::vector<int> _1_bin = {1, 0, 0, 0};
+    std::vector<int> _4_bin = {0, 0, 1, 0};
+    std::vector<int> _1_bin_additional_code = {1, 0, 0, 0};
+    std::vector<int> _4_bin_additional_code = {0, 0, 1, 0};
+
     std::vector<int> _minus_2_binary = {0, 1, 0, 1};
     std::vector<int> _minus_2_additional = {0, 1, 0, 1};
 
-    EXPECT_EQ(_2_additional, calc._2_to_additional_code(_2_binary));
+    EXPECT_EQ(_1_bin_additional_code, calc._2_to_additional_code(_1_bin));
+    EXPECT_EQ(_4_bin_additional_code, calc._2_to_additional_code(_4_bin));
     EXPECT_EQ(_minus_2_additional, calc._2_to_additional_code(_minus_2_binary));
 }
-
